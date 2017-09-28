600c8cfa50055592424cf261b54634a71c3969ec
Denisplusplus <whinslow@mail.ru>
2017-09-28 13:29:12 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 #include "StringMaker.h"
+#include "Vector.h"
+
+void menu(void);
+
 
 
 int main(void) {
 
-	char* string = StringCreate();
-	printf("%s\n", string);
-	StringDestroy(&string);
+	int choise;
+	TVector* vector = NULL;
+	vector = VectorCreate();
+
+    do {
+    	double key;
+		menu();
+		scanf("%d", &choise);
+		switch(choise) {
+			case 1 :
+				scanf("%lf", &key);
+				char* string = StringCreate();
+				VectorEntry(vector, key, string);
+				break;
+			case 2 :
+				VectorPrint(vector);
+				break;
+			case 3 :
+				VectorDestroy(&vector);
+				StringDestroy(&string);
+				break;
+			case 0 :
+				return 0;
+		}	
+	} while(1);	
 	return 0;
+}
+
+
+
+
+
+void menu(void)
+{
+	printf("1)Insert key and data to vector\n");
+	printf("2)Print vector\n");
+	printf("3)Destroy vector\n");
+
 }
