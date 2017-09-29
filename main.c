8b8d1377134f37873c3538cab83d52dfdc06e6b0
Denisplusplus <whinslow@mail.ru>
2017-09-29 10:26:32 -0700

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,8 @@ int main(void) {
 	int choise;
 	TVector* vector = NULL;
 	vector = VectorCreate();
-
+	TVector* vector_sorted = NULL;
+	//vector_sorted = VectorCreate();
     do {
     	double key;
 		menu();
@@ -27,8 +28,16 @@ int main(void) {
 				VectorPrint(vector);
 				break;
 			case 3 :
-				VectorDestroy(&vector);
+				vector_sorted = BucketSort(vector);
+				break;
+			case 4 :
+			   VectorPrint(vector);		
+				break;		
+			case 5 :
 				StringDestroy(&string);
+				VectorDestroy(&vector);
+				VectorDestroy(&vector_sorted);
+				
 				break;
 			case 0 :
 				return 0;
@@ -45,6 +54,8 @@ void menu(void)
 {
 	printf("1)Insert key and data to vector\n");
 	printf("2)Print vector\n");
-	printf("3)Destroy vector\n");
+	printf("3)Sort vector\n");
+	printf("4)Print sorted vector\n");
+	printf("5)Destroy vector\n");
 
 }
