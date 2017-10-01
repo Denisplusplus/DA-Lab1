1d30b6f757f1cbe25f24e0bc5f297af549b1c290
Denisplusplus <whinslow@mail.ru>
2017-10-01 12:23:26 -0700

    Update Vector.c

--- Vector.c
+++ Vector.c
@@ -1,44 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "StringMaker.h"
 #include "Vector.h"
+
+void swap(TVector* vector1,  TVector* vector2)
+{
+	TVector temp;
+	temp = *vector1;
+	*vector1 = *vector2;
+	*vector2 = temp;
+}
+
+TVector* InsertionSort(TVector* vector)
+{	
+	if (!vector || vector->size < 2) {
+		return vector;
+	}
+	for (int i = 1; i < vector->size; i++) {
+		for (int j = i; j > 0 && vector->item_array[j - 1].key > vector->item_array[j].key; j--) {
+			swap(&(vector->item_array[j - 1]), &(vector -> item_array[j]));
+		}
+	}
+	return vector;
+}
+
 
 TVector* BucketSort(TVector* vector) {
 	if (vector == NULL || vector->size < 2) {
 		return vector;
 	}
+	
 	double maxValue = vector->item_array[0].key; 
 	double minValue = vector->item_array[0].key;
 	for (int i = 1; i < vector->size; i++) {
 		if (vector->item_array[i].key > maxValue) maxValue = vector->item_array[i].key;
 		if (vector->item_array[i].key < minValue) minValue = vector->item_array[i].key;
 	}
-	int buckets_size = (int) (maxValue - minValue + 1);
-	TVector*  bucket[buckets_size];
-	for (int i = 0; i<buckets_size; i++) {
+
+	int bucket_size = vector->size / 2 ;
+	double length = (maxValue - minValue + 1) / bucket_size;
+
+	TVector*  bucket[bucket_size];
+	for (int i = 0; i < bucket_size; i++) {
 		bucket[i] = VectorCreate();
 	}
-   	for (int i = 0; i < vector->size; i++) {
- 		VectorEntry(bucket[(int) (vector->item_array[i].key - minValue)], vector->item_array[i].key, vector->item_array[i].data);
+
+	for (int i = 0; i < vector->size; i++) {
+       	int index = (int) ((vector->item_array[i].key - minValue) / length);
+        VectorEntry(bucket[index], vector->item_array[i].key, vector->item_array[i].data);
     }
-    TVector* vectorR = NULL;
-   	vectorR = VectorCreate();
-    for (int i = 0; i < buckets_size; i++) {
-        if (bucket[i]->size > 0) {
-            for (int j = 0; j < bucket[i]->size; j++) {
-               VectorEntry(vectorR, bucket[i]->item_array[j].key, bucket[i]->item_array[j].data);
-            }
-        }
+   
+    TVector* newVector = VectorCreate();
+    for (int i = 0; i < bucket_size; i++) {
+    	 InsertionSort(bucket[i]);
+    	 for (int j = 0; j < bucket[i]->size; j++) {
+    	 	VectorEntry(newVector, bucket[i]->item_array[j].key,  bucket[i]->item_array[j].data);
+    	 }
     }
 
-    for (int i = 0; i < buckets_size; i++) {
-    	VectorDestroy(&bucket[i]);
-    }	
-    if (*bucket) {
-    	free(*bucket);
-    	(*bucket) = NULL;
-    }
-    return vectorR;
+
+   return newVector; 
 }
 
 TVector* VectorCreate(void) {
@@ -57,19 +79,16 @@ void VectorEntry(TVector* vector, Key key, char *data) {
 
 
 void VectorPrint(TVector* vector) {
-    if ((vector->size == 0) || (!vector)) {
-        printf("Vector is empty\n");
-        return;
-    }
     for (int i = 0; i < vector->size; i++) {
-    		printf("%lf\n", vector->item_array[i].key);
+    		printf("%lf\t", vector->item_array[i].key);
         	printf("%s\n", vector->item_array[i].data);
-    
     }
-    printf("\n");
 }
 
 void VectorDestroy(TVector** vector) {
+	if (!(*vector) || ((*vector)->size == 0)) {
+		return;
+	}
 	int max_size = (*vector)->size - 1;
 	for (int i = 0; i < max_size; i++) {
 		if ((*vector)->item_array[i].data) {
@@ -82,7 +101,7 @@ void VectorDestroy(TVector** vector) {
 	}
 	if (*vector) {
 		free (*vector);
-		*vector = NULL;
+		(*vector) = NULL;
 	}
 
 }
